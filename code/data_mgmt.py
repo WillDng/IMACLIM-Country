0c91642043ce36dbccfda38fa6eb1ef3e955d113
will <dang@centre-cired.fr>
2019-01-29 14:56:54 +0100

    extract system linebreaker and directory separator and add a get_filename_from(path) method

--- code/data_mgmt.py
+++ code/data_mgmt.py
@@ -1,13 +1,23 @@
 # coding : utf-8
 
 import sys
+import os
 import pandas
 
+linebreaker = '\n'
+dir_separator = os.sep
+
 def import_IOT(IOT_file_path, **kwargs):
-	read_IOT = pandas.read_csv(IOT_file_path, index_col=0, **kwargs)
+	read_IOT = pandas.read_csv(IOT_file_path, 
+							   index_col=0,
+							   **kwargs)
 	if len(get_IOT_header_from(read_IOT)) < 2:
-		sys.stderr.write("Warning : IOT delimiter might not be correctly informed\n")
+		IOT_name = get_filename_from(IOT_file_path)
+		sys.stderr.write("Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker)
 	return read_IOT
 
 def get_IOT_header_from(IOT):
 	return IOT.columns.tolist()
+
+def get_filename_from(path):
+	return path.split(dir_separator)[-1]
--- tests/test_data_mgmt.py
+++ tests/test_data_mgmt.py
@@ -3,6 +3,7 @@
 import pytest
 import pandas
 from code import data_mgmt
+from code.data_mgmt import linebreaker, dir_separator
 
 good_IOT_path = 'tests/mock_data/IOT_Val.csv'
 IOT_delimiter = ';'
@@ -25,12 +26,17 @@ def test_import_IOT_non_correct_column_header():
 	read_IOT = data_mgmt.import_IOT(good_IOT_path)
 	assert all([isinstance(header, str) for header in read_IOT.index])
 
-def test_import_bad_delimiter_IOT(bad_delimiter_IOT, capsys):
+def test_get_filename_from_path():
+	filename = data_mgmt.get_filename_from(good_IOT_path)
+	assert filename == good_IOT_path.split(dir_separator)[-1]
+
+def test_import_bad_delimiter_IOT(capsys):
 	data_mgmt.import_IOT(good_IOT_path)
+	IOT_name = data_mgmt.get_filename_from(good_IOT_path)
 	captured = capsys.readouterr()
-	assert captured.err == "Warning : IOT delimiter might not be correctly informed\n"
+	assert captured.err == "Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker
 
 def test_get_IOT_header_from(good_IOT):
 	header = data_mgmt.get_IOT_header_from(good_IOT)
-	IOT_header = next(open(good_IOT_path)).rstrip('\n').split(IOT_delimiter)[1:]
+	IOT_header = next(open(good_IOT_path)).rstrip(linebreaker).split(IOT_delimiter)[1:]
 	assert header == IOT_header
