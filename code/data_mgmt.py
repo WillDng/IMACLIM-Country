5bfaa5f65f10fe709e4ebcecaef128d2bb3c090c
will <dang@centre-cired.fr>
2019-02-05 13:16:55 +0100

    change read_classification() to read_IOT_aggregation, function handles more than one column, change indentation to spaces

--- code/data_mgmt.py
+++ code/data_mgmt.py
@@ -9,49 +9,57 @@ linebreaker = '\n'
 dir_separator = os.sep
 
 def import_IOT(IOT_file_path, **kwargs):
-	read_IOT = pandas.read_csv(IOT_file_path, 
-							   index_col=0,
-							   **kwargs)
-	if len(get_IOT_header_from(read_IOT)) < 2:
-		IOT_name = get_filename_from(IOT_file_path)
-		sys.stderr.write("Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker)
-	return read_IOT
+    read_IOT = pandas.read_csv(IOT_file_path, 
+                               index_col=0,
+                               **kwargs)
+    if len(get_IOT_header_from(read_IOT)) < 2:
+        IOT_name = get_filename_from(IOT_file_path)
+        sys.stderr.write("Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker)
+    return read_IOT
 
 def get_IOT_header_from(IOT):
-	return IOT.columns.tolist()
+    return IOT.columns.tolist()
 
 def get_filename_from(path):
-	return path.split(dir_separator)[-1]
+    return path.split(dir_separator)[-1]
 
-def read_classification_from(IOT_classification_path, delimiter='|'):
-	reader = _get_reader_from(IOT_classification_path, delimiter)
-	IOT_classification = dict()
-	for row in reader:
-		try:
-			group = row[1]
-			if group not in IOT_classification:
-				IOT_classification[group] = set()
-			IOT_classification[group].add(row[0])
-		except IndexError:
-			sys.stderr.write("Warning : delimiter might not be correctly informed in function")
-			return None
-	return IOT_classification
+def read_IOT_aggregation_from(IOT_aggregation_path, delimiter='|'):
+    """ Hypothesis : in first column are the names of the individuals and in columns aggregates names """
+    reader = _get_reader_from(IOT_aggregation_path, delimiter)
+    IOT_aggregation = dict()
+    for individual_description in reader:
+        individual = individual_description[0]
+        aggregates = individual_description[1:]
+        if not aggregates:
+            sys.stderr.write("Warning : delimiter might not be correctly informed in function")
+            return            
+        for aggregate in aggregates:
+            if aggregate not in IOT_aggregation:
+                IOT_aggregation[aggregate] = list()
+            if individual not in IOT_aggregation[aggregate]:
+                IOT_aggregation[aggregate].append(individual)
+            else:
+                pass
+                #FIXME should raise warning ?
+    for aggregate, individuals in IOT_aggregation.items():
+        IOT_aggregation[aggregate] = tuple(individuals)
+    return IOT_aggregation
 
 def _get_reader_from(path, delimiter):
-	return csv.reader(open(path), delimiter=delimiter)
+    return csv.reader(open(path), delimiter=delimiter)
 
 def slice_(IOT, field_headers):
-	sliced_IOT = IOT.loc[field_headers]
-	if sliced_IOT.isnull().values.any():
-		sys.stderr.write("Warning : IOT headers might be ill informed"+linebreaker)
-	return sliced_IOT
+    sliced_IOT = IOT.loc[field_headers]
+    if sliced_IOT.isnull().values.any():
+        sys.stderr.write("Warning : IOT headers might be ill informed"+linebreaker)
+    return sliced_IOT
 
 def read_grouping_from(path, delimiter='|'):
-	reader = _get_reader_from(path, delimiter)
-	grouping = dict()
-	for row in reader:
-		group = row[0]
-		if group not in grouping:
-			grouping[group] = list()
-		grouping[group] = row[1:]
-	return grouping
+    reader = _get_reader_from(path, delimiter)
+    grouping = dict()
+    for row in reader:
+        group = row[0]
+        if group not in grouping:
+            grouping[group] = list()
+        grouping[group] = row[1:]
+    return grouping
--- tests/test_data_mgmt.py
+++ tests/test_data_mgmt.py
@@ -12,71 +12,81 @@ IOT_delimiter = ';'
 
 @pytest.fixture()
 def part_IOT():
-	return pandas.read_csv(part_IOT_path, 
-						   delimiter=IOT_delimiter,
-						   index_col=0)
+    return pandas.read_csv(part_IOT_path, 
+                           delimiter=IOT_delimiter,
+                           index_col=0)
 
 @pytest.fixture()
 def bad_delimiter_IOT():
-	return pandas.read_csv(part_IOT_path)
+    return pandas.read_csv(part_IOT_path)
 
 @pytest.fixture()
 def full_IOT():
-	return pandas.read_csv(full_IOT_path, 
-						   delimiter=IOT_delimiter,
-						   index_col=0)
+    return pandas.read_csv(full_IOT_path, 
+                           delimiter=IOT_delimiter,
+                           index_col=0)
 
 
 def test_import_IOT(part_IOT):
-	read_IOT = data_mgmt.import_IOT(part_IOT_path, delimiter=';')
-	assert read_IOT.equals(part_IOT)
+    read_IOT = data_mgmt.import_IOT(part_IOT_path, delimiter=';')
+    assert read_IOT.equals(part_IOT)
 
 def test_import_IOT_non_correct_column_header():
-	read_IOT = data_mgmt.import_IOT(part_IOT_path)
-	assert all([isinstance(header, str) for header in read_IOT.index])
+    read_IOT = data_mgmt.import_IOT(part_IOT_path)
+    assert all([isinstance(header, str) for header in read_IOT.index])
 
 def test_get_filename_from_path():
-	filename = data_mgmt.get_filename_from(part_IOT_path)
-	assert filename == part_IOT_path.split(dir_separator)[-1]
+    filename = data_mgmt.get_filename_from(part_IOT_path)
+    assert filename == part_IOT_path.split(dir_separator)[-1]
 
 def test_import_bad_delimiter_IOT(capsys):
-	data_mgmt.import_IOT(part_IOT_path)
-	IOT_name = data_mgmt.get_filename_from(part_IOT_path)
-	captured = capsys.readouterr()
-	assert captured.err == "Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker
+    data_mgmt.import_IOT(part_IOT_path)
+    IOT_name = data_mgmt.get_filename_from(part_IOT_path)
+    captured = capsys.readouterr()
+    assert captured.err == "Warning : IOT delimiter might not be correctly informed in "+IOT_name+linebreaker
 
 def test_get_IOT_header_from(part_IOT):
-	header = data_mgmt.get_IOT_header_from(part_IOT)
-	IOT_header = next(open(part_IOT_path)).rstrip(linebreaker).split(IOT_delimiter)[1:]
-	assert header == IOT_header
+    header = data_mgmt.get_IOT_header_from(part_IOT)
+    IOT_header = next(open(part_IOT_path)).rstrip(linebreaker).split(IOT_delimiter)[1:]
+    assert header == IOT_header
 
-classification_file_path = mock_data_dir+'IOT_classification_part.csv'
+IOT_aggregation_file_path = mock_data_dir+'IOT_aggregation_part.csv'
 
-def test_read_classification_from():
-	expected_dictionnary = {u'Commodities':set([u'Crude_oil', u'Natural_gas', u'Coking_coal']),
-							u'OthPart_IOT':set([u'Labour_income', u'Labour_Tax']),
-							u'Sectors':set([u'Crude_oil', u'Natural_gas', u'Coking_coal']),
-							u'FC':set([u'I', u'X'])}
-	IOT_classification = data_mgmt.read_classification_from(classification_file_path, 
-															delimiter=';')
-	assert IOT_classification == expected_dictionnary
+@pytest.fixture()
+def expected_IOT_aggregation():
+    mock_IOT_aggregation = {'Commodities':('Crude_oil', 'Natural_gas', 'Coking_coal'),
+                            'OthPart_IOT':('Labour_income', 'Labour_Tax'),
+                            'Sectors':('Crude_oil', 'Natural_gas', 'Coking_coal'),
+                            'FC':('I', 'X'),
+                            'EnerSect':('Crude_oil', 'Natural_gas', 'Coking_coal'),
+                            'Value_Added':('Labour_income', 'Labour_Tax'),
+                            'NonSupplierSect':('Crude_oil', 'Natural_gas', 'Coking_coal')}
+    return mock_IOT_aggregation
 
-def test_read_classification_raise_delimiter_warning(capsys):
-	data_mgmt.read_classification_from(classification_file_path)
-	captured = capsys.readouterr()
-	assert captured.err == "Warning : delimiter might not be correctly informed in function"
+def test_read_IOT_aggregation_from(expected_IOT_aggregation):
+    IOT_aggregation = data_mgmt.read_IOT_aggregation_from(IOT_aggregation_file_path, 
+                                                          delimiter=';')
+    assert IOT_aggregation == expected_IOT_aggregation
+
+def test_read_IOT_aggregation_raise_delimiter_warning(capsys):
+    data_mgmt.read_IOT_aggregation_from(IOT_aggregation_file_path)
+    captured = capsys.readouterr()
+    assert captured.err == "Warning : delimiter might not be correctly informed in function"
 
 #FIXME might need to inform type of slice e.g:IC
 def test_slice_warning_when_bad_headers(full_IOT, part_IOT, capsys):
-	wrong_headers = (['Coking_coal', 'I'], ['Natural_gas', 'Labour_Tax'])
-	data_mgmt.slice_(full_IOT, wrong_headers)
-	captured = capsys.readouterr()
-	assert captured.err == "Warning : IOT headers might be ill informed"+linebreaker
+    wrong_headers = (['Coking_coal', 'I'], ['Natural_gas', 'Labour_Tax'])
+    data_mgmt.slice_(full_IOT, wrong_headers)
+    captured = capsys.readouterr()
+    assert captured.err == "Warning : IOT headers might be ill informed"+linebreaker
 
 def test_read_grouping_from():
-	grouping_file_path = mock_data_dir + 'IOT_grouping.csv'
-	read_grouping = data_mgmt.read_grouping_from(grouping_file_path)
-	expected_grouping = {'IC':['Commodities','Sectors'],
-						 'FC':['Commodities', 'FC'],
-						 'OthPart_IOT':['OthPart_IOT', 'Sectors']}
-	assert read_grouping == expected_grouping
+    grouping_file_path = mock_data_dir + 'IOT_grouping.csv'
+    read_grouping = data_mgmt.read_grouping_from(grouping_file_path)
+    expected_grouping = {'IC':['Commodities','Sectors'],
+                         'FC':['Commodities', 'FC'],
+                         'OthPart_IOT':['OthPart_IOT', 'Sectors']}
+    assert read_grouping == expected_grouping
+
+# def test_change_classification_order(expected_classification):
+#   modified_classification = data_mgmt.change_classification_order(expected_classification, )
