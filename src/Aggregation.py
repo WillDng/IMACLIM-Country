81eb29299faf2c366cf415d12cd03c1ae5977e26
William Dang <dang@centre-cired.fr>
2019-03-26 12:52:30 +0100

    Use collections.defaultdict in extract_agg()

--- src/Aggregation.py
+++ src/Aggregation.py
@@ -3,7 +3,8 @@
 import pathlib as pl
 import src.common_utils as cu
 from src.paths import data_dir
-from typing import (Dict, Iterator)
+import collections
+from typing import (Dict, Iterator, List)
 import copy
 import ipdb
 
@@ -16,10 +17,13 @@ def extract_agg(raw_data: Iterator) -> Dict[str, Dict[str, str]]:
     agg_types = list(filter(None, raw_data.__next__()))
     for agg_type in agg_types:
         out_agg[agg_type] = dict()
+def extract_agg(raw_data: Iterator, agg_types: List[str]
+                ) -> Dict[str, Dict[str, str]]:
+    out_agg = collections.defaultdict(dict)
     for activity_row in raw_data:
         for agg_type_index, agg_type in enumerate(agg_types):
             out_agg[agg_type][activity_row[0]] = activity_row[agg_type_index+1]
-    return out_agg
+    return dict(out_agg)
 
 
 def complete_missing_keys(dict_to_commplete, headers):
