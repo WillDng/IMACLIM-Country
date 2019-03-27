03f026f0cac06e857bc4f649dfcd3c28e2258774
William Dang <dang@centre-cired.fr>
2019-03-27 14:14:58 +0100

    Remove test_extract_aggregation()

--- tests/test_Aggregation.py
+++ tests/test_Aggregation.py
@@ -2,16 +2,6 @@
 
 from src import Aggregation
 
-def test_extract_aggregation():
-    iter_aggregattion = iter([['','AGG_SNBC2','AGG_MetMin','AGG_IndEner'],
-                              ['a', 'b', 'c', 'd'],
-                              ['e', 'f', 'g', 'h'],
-                              ['i', 'j', 'k', 'l']])
-    expected_aggregation = {'AGG_SNBC2' : {'a':'b', 'e':'f', 'i':'j'},
-                            'AGG_MetMin': {'a':'c', 'e':'g', 'i':'k'},
-                            'AGG_IndEner': {'a':'d', 'e':'h', 'i':'l'}}
-    assert Aggregation.extract_agg(iter_aggregattion) == expected_aggregation
-
 
 def test_complete_missing_keys():
     headers = ['a', 'b', 'c', 'd']
