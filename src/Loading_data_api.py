2bd6cb7fd5dd63170e5c5c8e776b99150c7c8ab7
William Dang <dang@centre-cired.fr>
2019-04-08 12:03:43 +0200

    Extract use_categories from get_IOT_values() and simplify function

--- src/Loading_data_api.py
+++ src/Loading_data_api.py
@@ -3,35 +3,35 @@
 import pandas as pd
 import src.Loading_data_lib as ld
 import src.Aggregation as Agg
-from src.paths import (data_dir)
-from typing import (Dict, List, Tuple)
+from typing import (Dict, List, Tuple, Union)
+import ipdb
 
 Coordinates = Tuple[List[str], List[str]]
 
 
+use_categories = ['IC', 'FC']
+
+
 def get_IOT_values(study_dashb: Dict[str, str]
-                   ) -> Dict[str, pd.DataFrame]:
+                   ) -> (Dict[str, pd.DataFrame],
+                         Dict[str, List[str]],
+                         Union[Dict[str, str], None]):
     IOT_val = ld.read_table(study_dashb['studydata_dir'] / 'IOT_Val.csv',
                             delimiter=';')
-    common_activities_mapping_path = study_dashb['studydata_dir'] / 'common_activities_mapping.csv'
-    common_activities_mapping = ld.read_activities_mapping(common_activities_mapping_path,
+    common_activities_mapping = ld.read_activities_mapping(study_dashb['studydata_dir'] / 'common_activities_mapping.csv',
                                                            delimiter=',',
                                                            headers=ld.get_headers_from(IOT_val))
-    IOT_val, common_activities_mapping = Agg.apply_aggregation(study_dashb,
-                                                               IOT_val,
-                                                               common_activities_mapping)
+    IOT_val, common_activities_mapping, keys_aggregation = Agg.apply_value_aggregation(study_dashb,
+                                                                                       IOT_val,
+                                                                                       common_activities_mapping)
     value_activities_mapping = ld.extend_activities_mapping(study_dashb['studydata_dir'] / 'value_activities_mapping.csv',
                                                             IOT_val,
                                                             common_activities_mapping)
-    value_categories_coord = ld.read_categories_coordinates(study_dashb['studydata_dir'] / 'value_categories_coordinates.csv',
-                                                            delimiter=',')
-    value_coord = ld.map_categories_to_activities_coordinates(value_categories_coord,
-                                                              value_activities_mapping)
+    value_coord = ld.get_categories_coordinates(study_dashb['studydata_dir'] / 'value_categories_coordinates.csv',
+                                                value_activities_mapping)
     value_coord = ld.disaggregate_in_coordinates(value_coord,
                                                  ['FC', 'OthPart_IOT'], 'IC')
-
     value_ressource_categories = ['IC', 'OthPart_IOT']
-    use_categories = ['IC', 'FC']
     ld.is_IOT_balanced(use_categories, value_ressource_categories,
                        IOT_val, value_coord)
     current_ERE = ld.get_ERE(use_categories, value_ressource_categories,
@@ -46,4 +46,5 @@ def get_IOT_values(study_dashb: Dict[str, str]
                                  correction_condition, correction_value)
     ld.is_IOT_balanced(use_categories, value_ressource_categories,
                        IOT_val, value_coord)
-    return ld.extract_IOTs_from(IOT_val, value_coord), common_activities_mapping
+    return ld.extract_IOTs_from(IOT_val, value_coord), common_activities_mapping, keys_aggregation
+
