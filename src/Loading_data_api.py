1552216585c6dfbb851653195967c5e202f50873
William Dang <dang@centre-cired.fr>
2019-04-20 19:04:47 +0200

    Reorganize imports, rename ld alias for Loading_data_lib to ldl

--- src/Loading_data_api.py
+++ src/Loading_data_api.py
@@ -1,9 +1,9 @@
 # coding : utf-8
 
-import pandas as pd
 from src import (common_utils as cu,
                  Aggregation as Agg,
-                 Loading_data_lib as ld)
+                 Loading_data_lib as ldl)
+import pandas as pd
 from typing import (Dict, List, Tuple, Union)
 import ipdb
 
@@ -58,37 +58,40 @@ def get_IOT_quantities(study_dashb: Dict[str, str],
                        common_activities_mapping: Dict[str, List[str]],
                        keys_aggregation: Union[Dict[str, str], None]
                        ) -> Dict[str, pd.DataFrame]:
-    IOT_quantity = ld.read_table(study_dashb['studydata_dir'] / 'IOT_Qtities.csv',
-                                 delimiter=';',
-                                 skipfooter=1,
-                                 engine='python')
     if keys_aggregation:
         IOT_quantity = Agg.aggregate_IOT(IOT_quantity, keys_aggregation)
     quantity_activities_mapping = ld.extend_activities_mapping(study_dashb['studydata_dir'] / 'quantity_activities_mapping.csv',
                                                                IOT_quantity,
                                                                common_activities_mapping)
-    quantity_coord = ld.get_categories_coordinates(study_dashb['studydata_dir'] / 'quantity_categories_coordinates.csv',
-                                                   quantity_activities_mapping)
-    quantity_coord = ld.disaggregate_in_coordinates(quantity_coord,
-                                                    ['FC'], 'IC')
+    IOT_quantity = ldl.read_table(study_dashb['studydata_dir'] / 'IOT_Qtities.csv',
+                                  delimiter=';',
+                                  skipfooter=1,
+                                  engine='python')
+    quantity_activities_mapping = ldl.extend_activities_mapping(study_dashb['studydata_dir'] / 'quantity_activities_mapping.csv',
+                                                                IOT_quantity,
+                                                                common_activities_mapping)
+    quantity_coord = ldl.get_categories_coordinates(study_dashb['studydata_dir'] / 'quantity_categories_coordinates.csv',
+                                                    quantity_activities_mapping)
+    quantity_coord = ldl.disaggregate_in_coordinates(quantity_coord,
+                                                     ['FC'], 'IC')
     quantity_ressource_categories = ['M', 'Y']
-    ld.is_IOT_balanced(use_categories, quantity_ressource_categories,
-                       IOT_quantity, quantity_coord)
-    current_ERE = ld.get_ERE(use_categories, quantity_ressource_categories,
-                             IOT_quantity, quantity_coord)
+    ldl.is_IOT_balanced(use_categories, quantity_ressource_categories,
+                        IOT_quantity, quantity_coord)
+    current_ERE = ldl.get_ERE(use_categories, quantity_ressource_categories,
+                              IOT_quantity, quantity_coord)
     quantity_uses_to_correct = {'Y': (IOT_quantity.loc[quantity_coord['Y']] != 0,
                                       IOT_quantity.loc[quantity_coord['Y']] + current_ERE),
                                 'X': (IOT_quantity.loc[quantity_coord['Y']] == 0,
                                       IOT_quantity.loc[quantity_coord['X']] - current_ERE)}
     for use_to_correct, correction_info in quantity_uses_to_correct.items():
         correction_condition, correction_value = correction_info
-        ld.modify_activity_value(IOT_quantity, quantity_coord[use_to_correct],
-                                 correction_condition, correction_value)
-    ld.is_IOT_balanced(use_categories, quantity_ressource_categories,
-                       IOT_quantity, quantity_coord)
+        ldl.modify_activity_value(IOT_quantity, quantity_coord[use_to_correct],
+                                  correction_condition, correction_value)
+    ldl.is_IOT_balanced(use_categories, quantity_ressource_categories,
+                        IOT_quantity, quantity_coord)
 
-    return (ld.extract_IOTs_from(IOT_quantity,
-                                 quantity_coord),
+    return (ldl.extract_IOTs_from(IOT_quantity,
+                                  quantity_coord),
             quantity_coord)
 
 
@@ -96,58 +99,38 @@ def get_IOT_prices(study_dashb: Dict[str, str],
                    keys_aggregation: Union[Dict[str, str], None],
                    quantity_coord: Dict[str, Tuple[List[str], List[str]]]
                    ) -> Dict[str, pd.DataFrame]:
-    IOT_prices = ld.read_table(study_dashb['studydata_dir'] / 'IOT_Prices.csv',
-                               delimiter=';',
-                               skipfooter=1,
-                               engine='python')
+    IOT_prices = ldl.read_table(study_dashb['studydata_dir'] / 'IOT_Prices.csv',
+                                delimiter=';',
+                                skipfooter=1,
+                                engine='python')
     # if keys_aggregation:
     #     IOT_prices = Agg.aggregate_IOT(IOT_prices,
     #                                    keys_aggregation)
-    return ld.extract_IOTs_from(IOT_prices,
-                                quantity_coord)
-
-
-def get_IOT_CO2(study_dashb: Dict[str, str],
-                common_activities_mapping: Dict[str, List[str]],
-                keys_aggregation: Union[Dict[str, str], None],
-                ) -> Dict[str, pd.DataFrame]:
-    IOT_CO2 = ld.read_table(study_dashb['studydata_dir'] / 'IOT_CO2.csv',
-                            delimiter=';',
-                            skipfooter=1,
-                            engine='python')
-    if keys_aggregation:
-        IOT_CO2 = Agg.aggregate_IOT(IOT_CO2,
-                                    keys_aggregation)
-    CO2_activities_mapping = ld.extend_activities_mapping(study_dashb['studydata_dir'] / 'CO2_activities_mapping.csv',
-                                                          IOT_CO2,
-                                                          common_activities_mapping)
-    CO2_activities_coord = ld.get_categories_coordinates(study_dashb['studydata_dir'] / 'CO2_categories_coordinates.csv',
-                                                         CO2_activities_mapping)
-    return ld.extract_IOTs_from(IOT_CO2,
-                                CO2_activities_coord)
+    return ldl.extract_IOTs_from(IOT_prices,
+                                 quantity_coord)
 
 
 def get_account_table(study_dashb: Dict[str, str]
                       ) -> Dict[str, Union[pd.Series, float]]:
-    account_table = ld.read_table(study_dashb['studydata_dir'] / 'DataAccountTable.csv',
-                                  delimiter=';',
-                                  skipfooter=1,
-                                  engine='python')
+    account_table = ldl.read_table(study_dashb['studydata_dir'] / 'DataAccountTable.csv',
+                                   delimiter=';',
+                                   skipfooter=1,
+                                   engine='python')
     selected_accounts = cu.read_dict(study_dashb['studydata_dir'] / study_dashb['DataAccount_params'],
                                      value_col=1,
                                      delimiter=';')
-    return ld.extract_accounts(account_table,
-                               selected_accounts)
+    return ldl.extract_accounts(account_table,
+                                selected_accounts)
 
 
 def get_labour(study_dashb: Dict[str, str],
                keys_aggregation: Union[Dict[str, str], None]
                ) -> pd.Series:
-    IOT_labour = ld.read_table(study_dashb['studydata_dir'] / 'Labour.csv',
-                               delimiter=';',
-                               skipfooter=1,
-                               engine='python')
     if keys_aggregation:
+    IOT_labour = ldl.read_table(study_dashb['studydata_dir'] / 'Labour.csv',
+                                delimiter=';',
+                                skipfooter=1,
+                                engine='python')
         IOT_labour = Agg.aggregate_IOT(IOT_labour,
                                        keys_aggregation)
     return IOT_labour
@@ -155,13 +138,13 @@ def get_labour(study_dashb: Dict[str, str],
 
 def get_demography(study_dashb: Dict[str, str]
                    ) -> Dict[str, float]:
-    demography_table = ld.read_table(study_dashb['studydata_dir'] / 'Demography.csv',
-                                     delimiter=';',
-                                     skipfooter=1,
-                                     engine='python')
+    demography_table = ldl.read_table(study_dashb['studydata_dir'] / 'Demography.csv',
+                                      delimiter=';',
+                                      skipfooter=1,
+                                      engine='python')
     to_extract_demography = {line: demography_table.columns[0] for line in demography_table.index}
-    return ld.pick_selection(demography_table,
-                             to_extract_demography)
+    return ldl.pick_selection(demography_table,
+                              to_extract_demography)
 
 
 def get_import_rates(study_dashb: Dict[str, str],
@@ -169,14 +152,14 @@ def get_import_rates(study_dashb: Dict[str, str],
                      keys_aggregation: Union[Dict[str, str], None],
                      value_coord: Dict[str, Coordinates]
                      ) -> Dict[str, float]:
-    IOT_import_rate = ld.read_table(study_dashb['studydata_dir'] / 'IOT_Import_rate.csv',
-                                    delimiter=';')
     IOT_import_value = IOT_val * IOT_import_rate
     if keys_aggregation:
         IOT_import_rate = Agg.aggregate_IOT(IOT_import_value,
                                             keys_aggregation)
-    import_rate_coord = ld.map_list_to_dict(use_categories, value_coord)
-    import_rate_coord = ld.disaggregate_in_coordinates(import_rate_coord,
-                                                       ['FC'], 'IC')
-    return ld.extract_IOTs_from(IOT_import_rate,
-                                import_rate_coord)
+    IOT_import_rate = ldl.read_table(study_dashb['studydata_dir'] / 'IOT_Import_rate.csv',
+                                     delimiter=';')
+    import_value_coord = ldl.map_list_to_dict(use_categories, value_coord)
+    import_value_coord = ldl.disaggregate_in_coordinates(import_value_coord,
+                                                         ['FC'], 'IC')
+    return ldl.extract_IOTs_from(IOT_import_value,
+                                 import_value_coord)
